50f20d839a113586ca844b71b3a2734f3d18e012
Arcanyx <nikhilchaughule@gmail.com>
2016-12-14 22:03:32 +0530

    Updated Cakephp lib folder to 2.9.3 for compatibility with PHP 7

--- lib/Cake/Test/Case/Controller/Component/Auth/ControllerAuthorizeTest.php
+++ lib/Cake/Test/Case/Controller/Component/Auth/ControllerAuthorizeTest.php
@@ -22,7 +22,7 @@ App::uses('CakeRequest', 'Network');
 App::uses('CakeResponse', 'Network');
 
 /**
- * Class ControllerAuthorizeTest
+ * ControllerAuthorizeTest
  *
  * @package       Cake.Test.Case.Controller.Component.Auth
  */
@@ -45,14 +45,25 @@ class ControllerAuthorizeTest extends CakeTestCase {
 	}
 
 /**
+ * testControllerTypeError
+ *
  * @expectedException PHPUnit_Framework_Error
+ * @throws PHPUnit_Framework_Error
+ * @return void
  */
 	public function testControllerTypeError() {
-		$this->auth->controller(new StdClass());
+		try {
+			$this->auth->controller(new StdClass());
+		} catch (Throwable $t) {
+			throw new PHPUnit_Framework_Error($t);
+		}
 	}
 
 /**
+ * testControllerErrorOnMissingMethod
+ *
  * @expectedException CakeException
+ * @return void
  */
 	public function testControllerErrorOnMissingMethod() {
 		$this->auth->controller(new Controller());
@@ -85,4 +96,5 @@ class ControllerAuthorizeTest extends CakeTestCase {
 
 		$this->assertTrue($this->auth->authorize($user, $request));
 	}
+
 }
